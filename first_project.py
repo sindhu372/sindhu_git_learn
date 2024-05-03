94006e0d747a56c2d4b7415a8991715537805393
sindhu koramoni <koramonisindhu372@gmail.com>
2024-05-02 22:06:55 -0500

    2 version

--- first_project.py
+++ first_project.py
@@ -1,1 +1,4 @@
-print("first i created repo in git hub")
+1. first i created repo in git hub. and created one file first_project.py
+2. go to github repo --> code---> copy https link
+3. now go to git bash and execute below code
+    git clone https://github.com/sindhu372/sindhu_git_learn.git
